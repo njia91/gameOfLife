562b75d767dab32724aaa66dc1e296da9766200f
Michael Andersson <m-andersson@outlook.com>
2019-05-01 11:50:55 +0200

    Minor adjustments

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+import static java.lang.System.exit;
+
 /**
  * Tiny and simple Game of Life implementation with an simple GUI.
  *
@@ -9,26 +11,42 @@ public class main {
 
   public static void main(String[]args){
     Cell[][] gameBoard = null;
-    GameOfLife gol = new GameOfLife(Configuration.PULSAR);
     Scanner scanner = new Scanner(System.in);
-
-
+    GameOfLife gol = null;
     System.out.println("Welcome to the Game of Life");
+    System.out.println("Select initial configuration, enter an integer between 1 and 2\n" +
+            "1: Glider\n" +
+            "2: Pulsar");
+    String usrInput = scanner.nextLine();
+
+    switch (usrInput){
+      case "1":
+        gol = new GameOfLife(Configuration.GLIDER);
+        break;
+      case "2":
+        gol = new GameOfLife(Configuration.PULSAR);
+        break;
+      default:
+        System.err.println("Invalid input. Please choose an integer between 1 and 2.");
+        exit(1);
+    }
+
+
     do{
       gameBoard = gol.getCurrentGrid();
       displayGrid(gameBoard);
 
-      System.out.println("Press Enter to continue to next generation or Ctrl + C to terminate session");
+      System.out.println("Press Enter to continue to next generation or Ctrl + C to terminate program.");
       scanner.nextLine();
       gol.evolveToNextGeneration();
       // Flush Console
-      System.out.print("\033[H\033[2J");
-      System.out.flush();
     } while(true);
   }
 
 
   public static void displayGrid(Cell[][] gb){
+    System.out.print("\033[H\033[2J");
+    System.out.flush();
     for (int row = 0; row < gb.length; row++){
       for (int col = 0; col < gb[0].length; col++){
         String output = gb[row][col].isAlive() ? "0" : "-";
--- src/test/java/GameOfLifeTest.java
+++ src/test/java/GameOfLifeTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
-class Test_GameOfLife {
+class GameOfLifeTest {
 
   @org.junit.jupiter.api.BeforeEach
   void setUp() {
